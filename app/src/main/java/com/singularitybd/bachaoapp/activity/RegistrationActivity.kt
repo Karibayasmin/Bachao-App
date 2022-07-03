1f7fd2c9cf8e5a04142dcacfa5dc5a81408665eb
Kariba Yasmin <kariba.singularity@gmail.com>
2022-07-03 20:30:20 +0600

    Added spinner in registration activity

--- app/src/main/java/com/singularitybd/bachaoapp/activity/RegistrationActivity.kt
+++ app/src/main/java/com/singularitybd/bachaoapp/activity/RegistrationActivity.kt
@@ -2,11 +2,48 @@ package com.singularitybd.bachaoapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.singularitybd.bachaoapp.R
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
+
+    var userTypeList : ArrayList<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        setUserTypeDropDown()
+    }
+
+    private fun setUserTypeDropDown() {
+        userTypeList.add("Select user type")
+        userTypeList.add("shopkeeper")
+        userTypeList.add("general")
+        var spinnerItemAdapter: ArrayAdapter<String> =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, userTypeList)
+
+        userTypeSpinner.adapter = spinnerItemAdapter
+
+        userTypeSpinner.setSelection(0)
+
+        userTypeSpinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+
+                Toast.makeText(parent.getContext(), "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
     }
 }
